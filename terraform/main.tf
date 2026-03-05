05832e333c6d57ed491002a5c508d49c913d94c9
rawan aglan <rawanrawan1912@gmail.com>
2026-03-05 02:39:39 +0200

    Update Terraform configuration: simplified structure and changed region to eu-central-1

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,62 +1,141 @@
-data "aws_availability_zone" "example" {
-  name = "eu-central-1a"
+# Simple AWS Free Tier Infrastructure for Task Manager
+
+# Get latest Ubuntu AMI
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
 }
 
-
-resource "aws_vpc" "my_vpc" {
-  cidr_block = "172.16.0.0/16"
+# VPC
+resource "aws_vpc" "main" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "tf-example"
+    Name = "task-manager-vpc"
   }
 }
 
-resource "aws_subnet" "my_subnet" {
-  vpc_id            = aws_vpc.my_vpc.id
-  cidr_block        = "172.16.10.0/24"
-  availability_zone = "us-west-2a"
+# Internet Gateway
+resource "aws_internet_gateway" "main" {
+  vpc_id = aws_vpc.main.id
 
   tags = {
-    Name = "tf-example"
+    Name = "task-manager-igw"
   }
 }
 
-resource "aws_network_interface" "example" {
-  subnet_id   = aws_subnet.my_subnet.id
-  private_ips = ["172.16.10.100"]
+# Public Subnet
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
+  availability_zone       = "eu-central-1a"
 
   tags = {
-    Name = "primary_network_interface"
+    Name = "task-manager-subnet"
   }
 }
 
-resource "aws_instance" "example" {
-  ami           = "ami-005e54dee72cc1d00" # us-west-2
-  instance_type = "t2.micro"
+# Route Table
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
 
-  primary_network_interface {
-    network_interface_id = aws_network_interface.example.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.main.id
   }
 
-  credit_specification {
-    cpu_credits = "unlimited"
+  tags = {
+    Name = "task-manager-rt"
   }
 }
 
-resource "aws_default_security_group" "default" {
-  vpc_id = aws_vpc.mainvpc.id
+# Route Table Association
+resource "aws_route_table_association" "public" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public.id
+}
 
+# Security Group
+resource "aws_security_group" "main" {
+  name        = "task-manager-sg"
+  description = "Security group for Task Manager"
+  vpc_id      = aws_vpc.main.id
+
+  # SSH
   ingress {
-    protocol  = -1
-    self      = true
-    from_port = 0
-    to_port   = 0
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # HTTP
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # HTTPS
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # App Port
+  ingress {
+    from_port   = 3000
+    to_port     = 3000
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # Outbound
   egress {
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
+  tags = {
+    Name = "task-manager-sg"
+  }
+}
+
+# EC2 Instance
+resource "aws_instance" "main" {
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = "task-manager-key" # Change this to your key name
+  subnet_id              = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.main.id]
+
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp3"
+  }
+
+  tags = {
+    Name = "task-manager-instance"
+  }
+}
+
+# Outputs
+output "instance_public_ip" {
+  value = aws_instance.main.public_ip
+}
+
+output "application_url" {
+  value = "http://${aws_instance.main.public_ip}:3000"
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -2,12 +2,11 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 6.0"
+      version = "~> 5.0"
     }
   }
 }
 
-# Configure the AWS Provider
 provider "aws" {
-  region = "us-east-1"
+  region = "eu-central-1"
 }
